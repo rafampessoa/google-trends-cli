2e6329e645d549dc5062bd53edb18125d0204978
Nao-30 <mohammedunive55@gmail.com>
2025-05-02 10:38:30 +0300

    patched a weird error

--- tests/test_trends_api.py
+++ tests/test_trends_api.py
@@ -14,7 +14,8 @@ class TestTrendsClient:
     @patch("gtrends.trends_api.requests.Session")
     def test_init(self, mock_session, mock_trends):
         """Test client initialization."""
-        # client = TrendsClient()
+        client = TrendsClient()
+        print(client)
         assert mock_trends.called
         assert mock_session.called
 
